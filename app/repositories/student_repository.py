af1fb30c2d7e47a2b08b06ec974b50ec304dd50b
sorenagorgan <hasanhasanhaji@gmail.com>
2026-08-05 17:00:11 +0330

    Update student_repository.py
    add all modules here

--- app/repositories/student_repository.py
+++ app/repositories/student_repository.py
@@ -21,3 +21,51 @@ def create_student(db: Session, student: StudentCreate):
     db.refresh(db_student)
 
     return db_student
+
+def get_students(db: Session):
+    """
+    Retrieve all students from the database.
+    """
+    return db.query(Student).all()
+
+
+def get_student_by_id(db: Session, student_id: int):
+    """
+    Retrieve a student by their ID.
+    """
+    return db.query(Student).filter(Student.id == student_id).first()
+
+
+def update_student(db: Session, student_id: int, student_data: StudentCreate):
+    """
+    Update an existing student's information.
+    """
+    student = get_student_by_id(
+        db,
+        student_id
+    )
+
+    if student is None:
+
+        return None
+    
+    if student:
+        student.name = student_data.name
+        student.age = student_data.age
+        student.email = student_data.email
+
+        db.commit()
+        db.refresh(student)
+    return student
+
+def delete_student(db: Session, student_id: int):
+    """
+    Delete a student from the database.
+    """
+    student = get_student_by_id(db, student_id)
+    if student is None:
+        return None
+
+    db.delete(student)
+    db.commit()
+    return student
