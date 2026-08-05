b6f953055334e4f65fe1acfa17e6fd3941e59f63
sorenagorgan <hasanhasanhaji@gmail.com>
2026-08-05 17:11:32 +0330

    Update student.py

--- app/routers/student.py
+++ app/routers/student.py
@@ -1,61 +1,151 @@
 from fastapi import APIRouter
-from fastapi import HTTPException
-from sqlalchemy.orm import Session
 from fastapi import Depends
-from app.schemas.student import StudentCreate, StudentResponse
+from fastapi import HTTPException
+
+
+from sqlalchemy.orm import Session
+
+
 from app.database.database import get_db
+
+
+from app.schemas.student import (
+    StudentCreate,
+    StudentResponse,
+    StudentUpdate
+)
+
+
 from app.services.student_service import *
 
 
-# Create a router for student-related endpoints
+
 router = APIRouter()
 
-# Define a GET endpoint to retrieve a list of students
-@router.get("/students")
-def get_students():
-    """
-    Get all students API endpoint.
-    """
-    return get_students_service()
-
-@router.get("/students/{student_id}")
-def get_student_by_id(student_id: int):
-    """
-    Get student by ID API endpoint.
-    """
-    student = get_student_by_id_service(student_id)
-    if student:
-        return student
-    else:
-        raise HTTPException(status_code=404, detail="Student not found")
-
-# Define a POST endpoint to create a new student
-@router.post("/students", response_model=StudentResponse)
-def create_student(student: StudentCreate, db: Session = Depends(get_db)): 
-    """
-    Create student API endpoint.
-
-    Router responsibility:
-    Receive request and call service layer.
-    """
-
-    # Send request to service layer
-    created_student = create_student_service(db,student)
-    return created_student
-
-@router.delete("/students/{student_id}")
-def delete_student(student_id: int):
-
-    """
-    Delete student endpoint.
-    """
 
 
-    student = delete_student_service(student_id)
+@router.post(
+    "/students",
+    response_model=StudentResponse
+)
+def create_student(
+
+    student: StudentCreate,
+
+    db: Session = Depends(get_db)
+
+):
+
+    return create_student_service(
+        db,
+        student
+    )
 
 
-    # Student was not found.
+
+
+
+@router.get(
+    "/students",
+    response_model=list[StudentResponse]
+)
+def get_students(
+
+    db: Session = Depends(get_db)
+
+):
+
+    return get_students_service(db)
+
+
+
+
+
+@router.get(
+    "/students/{student_id}",
+    response_model=StudentResponse
+)
+def get_student(
+
+    student_id: int,
+
+    db: Session = Depends(get_db)
+
+):
+
+    student = get_student_service(
+        db,
+        student_id
+    )
+
+
     if student is None:
+
+        raise HTTPException(
+            status_code=404,
+            detail="Student not found"
+        )
+
+
+    return student
+
+
+
+
+
+@router.put(
+    "/students/{student_id}",
+    response_model=StudentResponse
+)
+def update_student(
+
+    student_id: int,
+
+    student: StudentUpdate,
+
+    db: Session = Depends(get_db)
+
+):
+
+    updated = update_student_service(
+        db,
+        student_id,
+        student
+    )
+
+
+    if updated is None:
+
+        raise HTTPException(
+            status_code=404,
+            detail="Student not found"
+        )
+
+
+    return updated
+
+
+
+
+
+@router.delete(
+    "/students/{student_id}"
+)
+def delete_student(
+
+    student_id:int,
+
+    db: Session = Depends(get_db)
+
+):
+
+    deleted = delete_student_service(
+        db,
+        student_id
+    )
+
+
+    if deleted is None:
 
         raise HTTPException(
             status_code=404,
@@ -65,7 +155,6 @@ def delete_student(student_id: int):
 
     return {
 
-        "message": "Student deleted",
+        "message":"Student deleted"
 
-        "student": student
     }
