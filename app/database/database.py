a532ad873f350ad4c2faaca30bcc8a2f624e98ea
sorenagorgan <hasanhasanhaji@gmail.com>
2026-08-05 17:43:26 +0330

    update .env

--- app/database/database.py
+++ app/database/database.py
@@ -1,14 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import DeclarativeBase
+from app.core.config import settings
 
-# PostgreSQL connection URL
-DATABASE_URL = (
-    "postgresql://postgres:0173276@localhost:5432/student_db"
-)
+
 
 # Create a SQLAlchemy engine
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    settings.DATABASE_URL
+)
 
 # Create a session factory
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
