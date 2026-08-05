ea9312f5e0025c7c44df935382fa324f83507733
sorenagorgan <hasanhasanhaji@gmail.com>
2026-08-05 10:56:51 +0330

    Update student.py

--- app/routers/student.py
+++ app/routers/student.py
@@ -2,3 +2,17 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+@router.get("/students")
+def get_students():
+    return {
+        "students": [
+            {
+                "id": 1,
+                "name": "Ali"
+            },
+            {
+                "id": 2,
+                "name": "Sara"
+            }
+        ]
+    }
